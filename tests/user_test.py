80ce8cd7aca4b0b1482626f2a027ab8380f6354e
BoGi <bogomolovog@mail.ru>
2022-11-04 09:23:56 +0300

    making first registration tests

--- tests/user_test.py
+++ tests/user_test.py
@@ -7,7 +7,8 @@ from selenium.webdriver.common.by import By
 fake = Faker()
 
 
-def test_registration(get_user_generator, setup):
+@pytest.mark.skip("can't extract json from selenium yet")
+def test_registration_selenium(get_user_generator, setup):
     user = get_user_generator
     visitor = {
         'name': user.result['name'],
@@ -21,7 +22,27 @@ def test_registration(get_user_generator, setup):
     driver.find_element(By.XPATH, "// button[text() = 'Далее']").click()
 
 
+@pytest.mark.parametrize('email, password', [
+    ('dsf', '45dsfsdf@#4334534543'),
+    (fake.email(), '23'),
+    (fake.email(), ''),
+    ('', '45dsfsdf@#4334534543'),
+    ('dsfsdf.ru', '45dsfsdf@#4334534543')
 
+])
+def test_registration(email, password, get_user_generator):
+    user = get_user_generator
+    user.set_email(email)
+    user.set_password(password).set_password_confirmation()
+    visitor = {
+        'name': user.result['name'],
+        'phone': user.result['phone'],
+        'email': user.result['email'],
+    }
+
+    vis_req = requests.post('https://stage.deepskills.ru/api/v1/visitors', json=visitor)
+    user_req = requests.post('https://stage.deepskills.ru/auth', json=user.result)
+    assert user_req.status_code != 200, 'Status code is not what we expected'
 
 
 
